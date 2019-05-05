a044132f4a9afea0f7f3e1ad831759a1972efea2
Nataly09 <natali.strashko@gmail.com>
2019-05-05 12:13:26 +0300

    UMP

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".slider_ump").slick({
+    
+})
+});
+
+
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+
+  });
